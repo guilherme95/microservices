19cd3def37e7a669ca48cad041ed9148c820219f
guilherme95 <guilhermemello62@gmail.com>
2021-09-08 20:17:12 -0300

    book service module

--- calculator/src/main/java/br/com/guilherme/controller/PersonController.java
+++ calculator/src/main/java/br/com/guilherme/controller/PersonController.java
@@ -7,10 +7,7 @@ import br.com.guilherme.model.Person;
 import br.com.guilherme.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,4 +28,18 @@ public class PersonController {
         return services.findById(id);
     }
 
+    @RequestMapping(method=RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person){
+        return services.create(person);
+    }
+
+    @RequestMapping(method=RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person){
+        return services.update(person);
+    }
+
+    @RequestMapping(value="/{id}", method=RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void delete(@PathVariable("id") String id){
+        services.delete(id);
+    }
 }
